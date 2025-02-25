79f541bdf1cfacd96b60a7dd200205ab75193d1a
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-26 00:00:32 +0600

    feat: add LoginPage and RegisterPage placeholder components to routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import AuthLayout from './layout/AuthLayout';
 import { Route } from 'react-router';
 import { Routes } from 'react-router';
+import LoginPage from './pages/auth/login/login-page';
+import RegisterPage from './pages/auth/register/register-page';
 
 const App: FC = () => {
   return (
@@ -10,8 +12,8 @@ const App: FC = () => {
       <Route path="about" element={<div>About</div>} />
 
       <Route element={<AuthLayout />}>
-        <Route path="login" element={<div>Login</div>} />
-        <Route path="register" element={<div>Register</div>} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
