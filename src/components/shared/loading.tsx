5aa94f8aaf79df3c74df883d1099b2d4479ce0fe
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-27 04:58:26 +0600

    refactor: add dynamic className on loading component

--- src/components/shared/loading.tsx
+++ src/components/shared/loading.tsx
@@ -1,8 +1,13 @@
+import { cn } from '@/lib/utils';
 import { FC } from 'react';
 
-const Loading: FC = () => {
+interface ILoadingProps {
+    className?: string;
+}
+
+const Loading: FC<ILoadingProps> = ({ className }) => {
     return (
-        <div className="flex items-center justify-center h-screen bg-white">
+        <div className={cn('flex items-center justify-center h-full bg-white', className)}>
             <div className="w-10 h-10 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
         </div>
     );
