3806fe2fdc0de197baa0622b02bd728a3c4c60ea
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-26 23:09:12 +0600

    feat: add toast notification system with Radix UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from './components/ui/toaster.tsx'
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <Toaster />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
