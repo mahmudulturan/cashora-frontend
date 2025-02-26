028d7528bbf6aa728e80b3ff4deb7ca26c1472c9
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-26 22:29:17 +0600

    refactor: adjust label margin for improved form styling

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -6,7 +6,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "font-heading leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 block font-bold mb-3",
+  "font-heading leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 block font-bold mb-1",
 )
 
 const Label = React.forwardRef<
