075227f74f6f32b08cddd2fb50fb02e6ab4459e3
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-26 17:18:34 +0600

    feat: define transaction type interfaces for application data modeling

---
+++ src/types/transaction.ts
@@ -0,0 +1,24 @@
+import { IUser } from "./user";
+
+export interface ITransaction extends Document {
+    sender: string | IUser;
+    receiver: string | IUser;
+    amount: number;
+    fees: ITransactionFees;
+    note?: string;
+    type: 'send_money' | 'cash_in' | 'cash_out' | 'agent_recharge' | 'agent_withdraw';
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface ITransactionFees {
+    transactionFee: number;
+    adminFee: number;
+    agentCommission: number;
+}
+
+export interface ITransactionPayload {
+    receiver: string;
+    amount: number;
+    note: string;
+}
