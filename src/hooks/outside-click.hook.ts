5503d14aa944d101fa3635bb611bddb8c685013b
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-27 01:36:36 +0600

    feat: add useOutsideClick hook for handling outside click events

---
+++ src/hooks/outside-click.hook.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+const useOutsideClick = (callback: () => void, isOpen: boolean) => {
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+            callback();
+        }
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isOpen]); // Re-run when isOpen changes
+
+    return ref;
+};
+
+export default useOutsideClick;
