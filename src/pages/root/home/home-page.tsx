6c0b3ee76ae18013975cac8ab298b4f266b07f1c
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-27 09:54:10 +0600

    refactor: improve conditional rendering for agent home page status

--- src/pages/root/home/home-page.tsx
+++ src/pages/root/home/home-page.tsx
@@ -9,10 +9,15 @@ const HomePage: FC = () => {
     return (
         <main className="wrapper space-y-8 pb-8">
             {user?.role === 'user' && <UserQuickActions />}
-            {user?.role === 'agent' && user?.status === 'pending' ? <div className="card-white p-6">
-                <h3 className='text-2xl font-bold'>Your account is pending to be approved by admin.</h3>
-                <p className='text-gray-600'>Please wait for admin to approve your account.</p>
-            </div> : <AgentQuickActions />}
+            {
+                user?.role === 'agent' && (user?.status === 'pending' ?
+                    <div className="card-white p-6">
+                        <h3 className='text-2xl font-bold'>Your account is pending to be approved by admin.</h3>
+                        <p className='text-gray-600'>Please wait for admin to approve your account.</p>
+                    </div>
+                    :
+                    <AgentQuickActions />)
+            }
             {
                 user?.status !== 'pending' && <RecentTransactions />
             }
