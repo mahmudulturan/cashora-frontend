23ce73f304a9ca737927e3e8d131d13672f1c9a1
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-27 09:06:47 +0600

    feat: add loading state and handle empty transactions in recent transactions

--- src/pages/root/home/components/recent-transactions.tsx
+++ src/pages/root/home/components/recent-transactions.tsx
@@ -1,34 +1,48 @@
+import Loading from '@/components/shared/loading';
 import { useGetMyTransactions } from '@/hooks/transaction.hook';
 import { IUser } from '@/types/user';
 import formatDate from '@/utils/formatDate';
 import { FC } from 'react';
 
 const RecentTransactions: FC = () => {
-    const { data: transactions } = useGetMyTransactions('page=1&limit=3');
+    const { data: transactions, isLoading } = useGetMyTransactions('page=1&limit=3');
+
+    if (isLoading) {
+        return <Loading className='h-[30vh]' />;
+    }   
+
     return (
         <section className="card-white p-6">
-            <h2 className="text-xl font-bold mb-6">Recent Transactions</h2>
-            <div className="space-y-4">
-                {transactions?.data.result.map((transaction) => {
-                    const receiver = transaction.receiver as IUser;
-                    const sender = transaction.sender as IUser;
-                    return (
-                        <div key={transaction._id} className="card-white p-4">
-                            <div className="flex justify-between items-center">
-                                <div>
-                                    <p className="font-bold">{transaction.type === 'send_money' ? 'Send Money' : transaction.type === 'cash_out' ? 'Cash Out' : transaction.type === 'cash_in' ? 'Cash In' : 'Agent Recharge'}</p>
-                                    <p className="text-sm">To: {receiver.phone}</p>
-                                    <p className="text-sm">From: {sender.phone}</p>
-                                </div>
-                                <div className="text-right">
-                                    <p className="font-bold">৳ {transaction.amount}</p>
-                                    <p className="text-sm">{formatDate(transaction.createdAt.toString())}</p>
-                                </div>
-                            </div>
+            {
+                transactions?.data?.result && transactions?.data?.result.length > 0 ? (
+                    <>
+                        <h2 className="text-xl font-bold mb-6">Recent Transactions</h2>
+                        <div className="space-y-4">
+                            {transactions?.data.result.map((transaction) => {
+                                const receiver = transaction.receiver as IUser;
+                                const sender = transaction.sender as IUser;
+                                return (
+                                    <div key={transaction._id} className="card-white p-4">
+                                        <div className="flex justify-between items-center">
+                                            <div>
+                                                <p className="font-bold">{transaction.type === 'send_money' ? 'Send Money' : transaction.type === 'cash_out' ? 'Cash Out' : transaction.type === 'cash_in' ? 'Cash In' : 'Agent Recharge'}</p>
+                                                <p className="text-sm">To: {receiver.phone}</p>
+                                                <p className="text-sm">From: {sender.phone}</p>
+                                            </div>
+                                            <div className="text-right">
+                                                <p className="font-bold">৳ {transaction.amount}</p>
+                                                <p className="text-sm">{formatDate(transaction.createdAt.toString())}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    )
-                })}
-            </div>
+                    </>
+                ) : (
+                    <div className='text-xl font-bold'>No transactions found</div>
+                )
+            }
         </section>
     );
 };
