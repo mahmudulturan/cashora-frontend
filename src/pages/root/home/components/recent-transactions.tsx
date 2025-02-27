d7a38cc3b948296716a204492eb2b227218c83f5
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-27 08:28:42 +0600

    feat: show recent transactions data

--- src/pages/root/home/components/recent-transactions.tsx
+++ src/pages/root/home/components/recent-transactions.tsx
@@ -1,24 +1,33 @@
+import { useGetMyTransactions } from '@/hooks/transaction.hook';
+import { IUser } from '@/types/user';
+import formatDate from '@/utils/formatDate';
 import { FC } from 'react';
 
 const RecentTransactions: FC = () => {
+    const { data: transactions } = useGetMyTransactions('page=1&limit=3');
     return (
         <section className="card-white p-6">
             <h2 className="text-xl font-bold mb-6">Recent Transactions</h2>
             <div className="space-y-4">
-                {[1, 2, 3].map((i) => (
-                    <div key={i} className="card-white p-4">
-                        <div className="flex justify-between items-center">
-                            <div>
-                                <p className="font-bold">Send Money</p>
-                                <p className="text-sm">To: 01712345678</p>
-                            </div>
-                            <div className="text-right">
-                                <p className="font-bold">৳ 500.00</p>
-                                <p className="text-sm">2 hours ago</p>
+                {transactions?.data.result.map((transaction) => {
+                    const receiver = transaction.receiver as IUser;
+                    const sender = transaction.sender as IUser;
+                    return (
+                        <div key={transaction._id} className="card-white p-4">
+                            <div className="flex justify-between items-center">
+                                <div>
+                                    <p className="font-bold">{transaction.type === 'send_money' ? 'Send Money' : transaction.type === 'cash_out' ? 'Cash Out' : transaction.type === 'cash_in' ? 'Cash In' : 'Agent Recharge'}</p>
+                                    <p className="text-sm">To: {receiver.phone}</p>
+                                    <p className="text-sm">From: {sender.phone}</p>
+                                </div>
+                                <div className="text-right">
+                                    <p className="font-bold">৳ {transaction.amount}</p>
+                                    <p className="text-sm">{formatDate(transaction.createdAt.toString())}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                })}
             </div>
         </section>
     );
