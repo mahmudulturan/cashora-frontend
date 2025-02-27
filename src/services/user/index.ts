1f4029e6c4844fdef47276c292dc3bfd659f580d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-27 06:53:48 +0600

    feat: add comprehensive user management service methods

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -2,10 +2,29 @@ import axiosInstance from "@/lib/axiosInstances";
 import { IResponseWithData } from "@/types/response";
 import { IUser } from "@/types/user";
 
-export const getUser = async () : Promise<IResponseWithData<IUser>> => {
+export const getUser = async (): Promise<IResponseWithData<IUser>> => {
     const response = await axiosInstance.get('/user/me');
     return response.data;
 }
 
+export const getAllUsers = async (): Promise<IResponseWithData<IUser[]>> => {
+    const response = await axiosInstance.get('/user/all');
+    return response.data;
+}
+
+export const updateUser = async (data: Partial<IUser>): Promise<IResponseWithData<IUser>> => {
+    const response = await axiosInstance.put('/user/me', data);
+    return response.data;
+}
+
+export const updateUserStatus = async (userId: string, status: string): Promise<IResponseWithData<IUser>> => {
+    const response = await axiosInstance.patch(`/user/status/${userId}`, { status });
+    return response.data;
+}
+
+export const deleteUser = async (userId: string): Promise<IResponseWithData<IUser>> => {
+    const response = await axiosInstance.delete(`/user/${userId}`);
+    return response.data;
+}
 
 
